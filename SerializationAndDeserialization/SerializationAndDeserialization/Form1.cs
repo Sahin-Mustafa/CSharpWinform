90cc112c2b8f5abd5633259a2e0126c4c0f2d2a9
Mustafa Sahin <Sahin-Mustafa@gmail.com>
2022-11-23 22:37:53 +0300

    Person Page btn events

--- SerializationAndDeserialization/SerializationAndDeserialization/Form1.cs
+++ SerializationAndDeserialization/SerializationAndDeserialization/Form1.cs
@@ -1,20 +1,105 @@
+using System.Text.Json;
+using static System.Windows.Forms.LinkLabel;
+
 namespace SerializationAndDeserialization
 {
     public partial class Form1 : Form
     {
+        List<Person> persons = new List<Person>();
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtFullName.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Kiþi Bilgileri Eksik");
+                return;
+            }
+            AddNewPerson();
+            LoadListboxMethod();
+            MessageBox.Show("Kiþi Baþarý ile Eklendi.");
+        }
+
         private void btnOpen_Click(object sender, EventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
 
+
+            openFileDialog.Title = "Open Files";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog.Filter = "File Format(*.json)|*.json";
+
+
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string json = File.ReadAllText(openFileDialog.FileName);
+                persons = JsonSerializer.Deserialize<List<Person>>(json);
+
+                LoadListboxMethod();
+            }
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
+            if(persons.Count== 0)
+            {
+                MessageBox.Show("Kiþi Listesi Boþ. Kayýt iþlemi baþarýsýz.");
+                return;
+            }
+            string json = JsonSerializer.Serialize(persons);
 
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Title = "Record to data";
+            saveFileDialog.OverwritePrompt = true;
+            saveFileDialog.RestoreDirectory = true;
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            saveFileDialog.Filter = "File Format(*.json)|*.json";
+
+            DialogResult result = saveFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                //File.WriteAllLines(saveFileDialog.FileName, lines);
+                File.WriteAllText(saveFileDialog.FileName, json);
+                MessageBox.Show("Data recorded");
+            }
         }
+
+        private void AddNewPerson()
+        {
+            Person person = new();
+            person.Fullname = txtFullName.Text;
+            person.Tc = txtTc.Text;
+            person.BirthDate = dbdBirthDate.Value;
+            person.Email = txtEmail.Text;
+            person.Phone = txtPhone.Text;
+            person.Gender = rdbFemale.Checked;
+            person.Country = txtCountry.Text;
+            person.RegionCode = txtRegionCode.Text;
+            person.City = txtPersonCity.Text;
+            person.District = txtDistrict.Text;
+            person.PostalCode = txtPostalCode.Text;
+            person.Street = txtStreet.Text;
+            person.CompanyName = txtCompanyName.Text;
+            person.CompanyCountry = txtCompanyCountry.Text;
+            person.CompanyCity = txtCompanyCity.Text;
+            person.ExperienceDesc = txtExperienceDesc.Text;
+
+            persons.Add(person);
+        }
+
+        private void LoadListboxMethod()
+        {
+            listBox1.DataSource = null;
+            listBox1.DataSource = persons;
+        }
+
+        
     }
 }
---
+++ SerializationAndDeserialization/SerializationAndDeserialization/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationAndDeserialization
+{
+    public class Person
+    {
+        public string Fullname { get; set; }
+        public string Tc { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool Gender { get; set; }
+        public string Country { get; set; }
+        public string RegionCode { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string PostalCode { get; set; }
+        public string Street { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyCountry { get; set; }
+        public string CompanyCity { get; set; }
+        public string ExperienceDesc { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Fullname} {this.Tc}";
+        }
+    }
+}
