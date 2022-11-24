03e2bd0dd0733e1db9ca6a54d5a015496a7c991d
Mustafa Sahin <ms.sahinmustafa@gmail.com>
2022-11-24 09:47:03 +0300

    Edit User

---
+++ SerializationAndDeserialization/SerializationAndDeserialization/User/Address.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationAndDeserialization
+{
+    public class Address
+    {
+        public Country Country { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string PostalCode { get; set; }
+        public string Street { get; set; }
+    }
+    public class Country
+    {
+        public string Name { get; set; }
+        public string RegionCode { get; set; }
+    }
+}
