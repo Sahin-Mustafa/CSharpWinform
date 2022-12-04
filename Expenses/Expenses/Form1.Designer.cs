b80b48242b92e976506b6b0ab184745674ddc92b
Mustafa Sahin <Sahin-Mustafa@gmail.com>
2022-12-04 15:23:19 +0300

    Revert "Delete Expenses directory"
    This reverts commit 0ea0a3ac0bcf8f5963c8e2ac567b3477b2bba71d.

---
+++ Expenses/Expenses/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Expenses
+{
+    partial class formLoginPage
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formLoginPage));
+            this.PanelBase = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSignUp = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.PanelBase.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelBase
+            // 
+            this.PanelBase.Controls.Add(this.pictureBox1);
+            this.PanelBase.Controls.Add(this.btnLogin);
+            this.PanelBase.Controls.Add(this.panel2);
+            this.PanelBase.Controls.Add(this.txtPassword);
+            this.PanelBase.Controls.Add(this.txtName);
+            this.PanelBase.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelBase.Location = new System.Drawing.Point(0, 0);
+            this.PanelBase.Name = "PanelBase";
+            this.PanelBase.Size = new System.Drawing.Size(288, 301);
+            this.PanelBase.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Expenses.Properties.Resources.user_128px;
+            this.pictureBox1.Location = new System.Drawing.Point(48, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(195, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnLogin.Location = new System.Drawing.Point(143, 238);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 33);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnSignUp);
+            this.panel2.Location = new System.Drawing.Point(46, 238);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(80, 33);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.AutoSize = true;
+            this.btnSignUp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSignUp.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.btnSignUp.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnSignUp.Location = new System.Drawing.Point(0, 0);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(83, 30);
+            this.btnSignUp.TabIndex = 1;
+            this.btnSignUp.Text = "Sign up";
+            this.btnSignUp.Click += new System.EventHandler(this.btnSignUp_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(46, 209);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.PlaceholderText = "Password";
+            this.txtPassword.Size = new System.Drawing.Size(197, 23);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(46, 166);
+            this.txtName.Name = "txtName";
+            this.txtName.PlaceholderText = "User Name";
+            this.txtName.Size = new System.Drawing.Size(197, 23);
+            this.txtName.TabIndex = 2;
+            this.txtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // formLoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 301);
+            this.Controls.Add(this.PanelBase);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "formLoginPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.formLoginPage_Load);
+            this.PanelBase.ResumeLayout(false);
+            this.PanelBase.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel PanelBase;
+        private Button btnLogin;
+        private Panel panel2;
+        private Label btnSignUp;
+        private TextBox txtPassword;
+        private TextBox txtName;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Expenses/Readme.md
@@ -0,0 +1,30 @@
+# Expense Application
+In this application, expense transactions were made according to the task types of the personnel in a company.
+
+
+## Login page
+Registered personnel can login with their username and password. Personnel information is kept in a Json file. New staff can be created. Each staff member has a unique number.
+
+## Home Page
+
+The page where the personnel can take action according to their duties
+
+### Worker
+
+ - Can only see their own spending.
+ - The status of the expenses it adds are recorded as pending.
+ - It can delete and update its approved or paid expenditures.
+
+### Manager
+
+ - It can see the expenses other than the expenses that have been paid for.
+ - The expenses entered are registered as approved. 
+ - It can only update or delete her own expenses and outstanding expenses.
+ - It can approve or reject the expenditures of worker type personnel.
+
+### Accountant
+ - It can see all Expenditures.
+ - Pays for approved expenses
+ 
+ #
+Operations on the listed expenditures are made with a right click.
